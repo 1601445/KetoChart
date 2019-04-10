f62ce36dcb4c25534dfbce86e3658fa42d08c3e0
1601445 <38161145+1601445@users.noreply.github.com>
2019-04-10 17:17:23 +0100

    Add files via upload

---
+++ KetoGraph/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KetoGraph
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void load(object sender, EventArgs e)
+        {
+            string[,] args = new string[100, 3];
+
+            args = readCVS((string[,])args);
+
+            graficator((string[,])args);
+        }
+
+        private void Sair(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
